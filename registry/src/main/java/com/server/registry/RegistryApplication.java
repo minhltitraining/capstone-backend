cbdd3836a8bb59b5d5ea084dfb657315b18c473d
NYLTI <139362797+NYLTI@users.noreply.github.com>
2023-08-09 12:22:24 -0400

    Add files via upload

---
+++ registry/src/main/java/com/server/registry/RegistryApplication.java
@@ -0,0 +1,37 @@
+package com.server.registry;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@SpringBootApplication
+@EnableEurekaServer
+@CrossOrigin("*")
+public class RegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryApplication.class, args);
+	}
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("OPTIONS");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("DELETE");
+	    config.addAllowedMethod("PATCH");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter();
+	}
+
+}
